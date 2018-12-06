8d1653fd84ea7436f031acd0692984ecc2a7a0e6
Valtteri Tarvus <valtteri6@msn.com>
2018-12-06 08:20:52 +0200

    Removed unused code and changed some seqs to list for performance

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,32 +1,26 @@
 ﻿open System
 open Utils
+open System.Diagnostics
 
 type Claim = {
   id : int
-  coords : (int*int) seq
+  coords : (int*int) list
 }
 
 let claim (array : int array) =
-  let createSeq from size = seq { from .. size+from-1}
+  let createList from size = [ from .. size+from-1 ]
   { id = array.[0]
-    coords = Seq.allPairs (createSeq array.[1] array.[3]) (createSeq array.[2] array.[4]) }
-
-let part1 : Claim seq -> int =
-  Seq.collect (fun claim -> claim.coords)
-  >> Seq.groupBy id
-  >> Seq.filter (fun (_, value) -> Seq.length value > 1)
-  >> Seq.length
+    coords = List.allPairs (createList array.[1] array.[3]) (createList array.[2] array.[4])}
   
 let part2 singleClaimList =
   let singleClaims = singleClaimList |> List.map fst |> Set.ofList
-  Seq.filter (fun claim -> Set.isSubset (claim.coords |> Set.ofSeq) singleClaims)
+  Seq.filter (fun claim -> Set.isSubset (claim.coords |> Set.ofList) singleClaims)
   >> Seq.map (fun claim -> claim.id)
   >> Seq.head
 
 let splitByClaimCount =
-  Seq.collect (fun claim -> claim.coords)
-  >> Seq.groupBy id
-  >> Seq.toList
+  List.collect (fun claim -> claim.coords)
+  >> List.groupBy id
   >> List.partition(fun (_, value) -> Seq.length value > 1)
 
 [<EntryPoint>]
@@ -34,11 +28,11 @@ let main argv =
   let claims =
     "input.txt"
     |> System.IO.File.ReadAllLines
-    |> Seq.map (Parser.getInts >> claim)
+    |> Array.toList
+    |> List.map (Parser.getInts >> claim)
 
   let multiClaims, singleClaims = splitByClaimCount claims
   multiClaims.Length |> printfn "Part 1: %i"
   claims |> (part2 singleClaims) |> printfn "Part 2: %i"
-
   Console.ReadKey() |> ignore
   0
